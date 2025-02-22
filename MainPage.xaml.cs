87e417ceef84506a2271eea8d5724ab3e06e8909
gdemartino93 <g.demartino9393@gmail.com>
2025-02-22 01:54:57 +0100

    used external resources and added logic

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,24 +1,71 @@
-﻿namespace MAUI_Citazioni
+﻿using System.Drawing;
+using Color = Microsoft.Maui.Graphics.Color;
+
+namespace MAUI_Citazioni
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
+        Random random = new Random();
+        List<string> quotes = new();
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void btnGenerateQuote_Clicked(object sender, EventArgs e)
         {
-            count++;
+            grdBackground.Background = GetGradientBackground();
+            lblQuote.Text = GetRandomQuote();
+        }
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+        private void GetBackgroundAndQuote()
+        {
+            grdBackground.Background = GetGradientBackground();
+            lblQuote.Text = GetRandomQuote();
+        }
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+        private LinearGradientBrush GetGradientBackground()
+        {
+            System.Drawing.Color startColor = System.Drawing.Color.FromArgb(random.Next(256), random.Next(256), random.Next(256));
+            System.Drawing.Color endColor = System.Drawing.Color.FromArgb(random.Next(256), random.Next(256), random.Next(256));
+
+            var colors = ColorUtility.ColorControls.GetColorGradient(startColor, endColor, 5);
+            float stopOffset = .0f;
+            GradientStopCollection stops = new GradientStopCollection();
+
+            foreach (var color in colors)
+            {
+                stops.Add(new GradientStop(Color.FromArgb(color.Name),
+                    stopOffset));
+                stopOffset += .2f;
+            }
+
+            return new LinearGradientBrush(stops, new Microsoft.Maui.Graphics.Point(0, 0), new Microsoft.Maui.Graphics.Point(1, 1));
+        }
+
+        private string GetRandomQuote()
+        {
+            var index = random.Next(quotes.Count);
+            return quotes[index];
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+            await LoadMauiAsset();
+            GetBackgroundAndQuote();
+        }
+
+        async Task LoadMauiAsset()
+        {
+            using var stream = await FileSystem.OpenAppPackageFileAsync("quotes.txt");
+            using var reader = new StreamReader(stream);
+
+
+            while (reader.Peek() != -1)
+            {
+                quotes.Add(reader.ReadLine());
+            }
         }
     }
 
--- MauiProgram.cs
+++ MauiProgram.cs
@@ -13,6 +13,8 @@ namespace MAUI_Citazioni
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Autography.otf", "AuthorFont");
+                    fonts.AddFont("Montserrat-Italic-VariableFont_wght", "QuoteFont");
                 });
 
 #if DEBUG
